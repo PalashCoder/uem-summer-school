02b3334036171d0dc3cd3b29c821aa3f3728dab5
Avik Agarwala <avikagarwala@outlook.com>
2024-04-08 20:55:30 +0530

    correct hero section

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { Button, Card, Typography } from "@material-tailwind/react";
 import Image from "next/image";
-import { Button, Typography, Card } from "@material-tailwind/react";
 
 function Hero() {
   return (
@@ -9,60 +9,30 @@ function Hero() {
       <Image
         width={1200}
         height={1200}
-        src="/image/image8.svg"
+        src="/image/uem.png"
         alt="bg-img"
+        priority = {true}
         className="absolute inset-0 ml-auto w-[920px] h-[780px] rounded-bl-[100px] object-cover object-center"
       />
       <div className="container mx-auto mt-28">
         <div className="grid grid-cols-12 text-center lg:text-left">
-          <Card className="col-span-full rounded-xl border border-white bg-white/90 py-10 p-8 shadow-lg shadow-black/10 backdrop-blur-sm backdrop-saturate-200 xl:col-span-7">
+          <Card className="mt-20 col-span-full rounded-xl border border-white bg-white/90 py-10 p-8 shadow-lg shadow-black/10 backdrop-blur-sm backdrop-saturate-200 xl:col-span-7">
             <Typography
               variant="h1"
               color="blue-gray"
               className="lg:text-5xl !leading-snug text-3xl lg:max-w-3xl"
             >
-              Unlock the Power of the Web with Our Expert Courses
+              Summer School
             </Typography>
             <Typography variant="lead" className="mb-10 mt-6 !text-gray-900">
-              Are you ready to embark on an exciting journey into the world of
-              web development? Look no further! We are your trusted partner for
-              mastering the art of web development.
+              Summer School Program of University of Engineering and Management,
+              Kolkata. Embrace the school student to a new horizon of education.
             </Typography>
             <div className="mb-8 flex justify-center gap-4 lg:justify-start">
               <Button color="gray">view all courses</Button>
-              <Button color="gray" variant="outlined">
+              {/* <Button color="gray" variant="outlined">
                 see pricing
-              </Button>
-            </div>
-            <div className="grid grid-cols-2 lg:grid-cols-4 items-center justify-between gap-4 lg:justify-start">
-              <Image
-                width={144}
-                height={144}
-                className="w-36 grayscale opacity-60"
-                src="/logos/logo-pinterest.svg"
-                alt="pinterest"
-              />
-              <Image
-                width={144}
-                height={144}
-                className="w-36 grayscale opacity-60"
-                src="/logos/logo-netflix.svg"
-                alt="netflix"
-              />
-              <Image
-                width={144}
-                height={144}
-                className="w-36 grayscale opacity-60"
-                src="/logos/logo-coinbase.svg"
-                alt="coinbase"
-              />
-              <Image
-                width={144}
-                height={144}
-                className="w-36 grayscale opacity-60"
-                src="/logos/logo-google.svg"
-                alt="google"
-              />
+              </Button> */}
             </div>
           </Card>
         </div>
