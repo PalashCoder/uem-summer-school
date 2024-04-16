36eb4b6465a9d0876ac3d80a8cfdebaa3f145ff8
Palash <ng.palash.das@gmail.com>
2024-04-16 19:59:02 +0530

    directory structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Footer from "../components/Footer/Footer.tsx";
 import Navbar from "../components/Navbar/Navbar.tsx";
-import AllCourses from "./AllCourses.tsx";
-import CoursesCategories from "./CoursesCategories.tsx";
-import Hero from "./Hero.tsx";
-import OurInstructors from "./OurInstructors.tsx";
-import PartnerCompanies from "./PartnerCompanies.tsx";
-import Stats from "./Stats.tsx";
+import AllCourses from "../components/Courses/AllCourses.tsx";
+import CoursesCategories from "../components/Courses/CoursesCategories.tsx";
+import Hero from "../components/Home/Hero.tsx";
+import OurInstructors from "../components/Instructors/OurInstructors.tsx";
+import PartnerCompanies from "../components/Partners/PartnerCompanies.tsx";
+import Stats from "../components/Home/Stats.tsx";
 
 export default function Campaign() {
   return (
